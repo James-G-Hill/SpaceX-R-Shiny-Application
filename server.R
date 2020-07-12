49d214b4b74a8f08cf2e16e3ccfe6ae35e23cee0
James Hill <j.g.hill@zoho.com>
2020-07-12 21:33:36 +0100

    Added code for the structure of the Data tab

--- server.R
+++ server.R
@@ -6,8 +6,20 @@ source(file = file.path("R", "utils_get_data.R"), local = TRUE)
 
 server <- function(input, output) {
   
-  lst_company <- shiny::reactive({ get_api_company() })
+  # Tables
+  tbl_launches <- shiny::reactive({ get_api_launches() })
+  tbl_launchpads <- shiny::reactive({ get_api_launchpads() })
+  tbl_rockets <- shiny::reactive({ get_api_rockets() })
   
+  # Data Tab
+  lst_tbls <- reactiveValues()
+  lst_tbls$launches <- tbl_launches
+  lst_tbls$launchpads <- tbl_launchpads
+  lst_tbls$rockets <- tbl_rockets
+  data_Server(ns_data, lst_tbls)
+  
+  # About Tab
+  lst_company <- shiny::reactive({ get_api_company() })
   about_Server(ns_about, lst_company)
   
 }
