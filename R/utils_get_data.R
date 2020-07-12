7d8ff31b72d6756b3eb9d3b409b3a88b89f48b44
James Hill <j.g.hill@zoho.com>
2020-07-12 21:18:44 +0100

    Added functions for returning more tables

--- R/utils_get_data.R
+++ R/utils_get_data.R
@@ -3,6 +3,9 @@
 url_base <- "https://api.spacexdata.com/v4"
 
 url_company <- glue::glue("{url_base}/company")
+url_launches <- glue::glue("{url_base}/launches")
+url_launchpads <- glue::glue("{url_base}/launchpads")
+url_rockets <- glue::glue("{url_base}/rockets")
 
 # Functions --------------------------------------------------------------------
 
@@ -24,5 +27,83 @@ get_api_data <- function(url) {
 get_api_company <- function() {
   
   get_api_data(url_company)
+  
+}
+
+#' GET 'launches' data from the SpaceX API
+#' 
+#' @return A list of the 'launches' data points.
+#' 
+get_api_launches <- function() {
+  
+  get_api_data(url_launches) %>%
+    dplyr::mutate(
+      crewed = purrr::map(crew, length) > 0,
+      flight_date = as.Date(date_utc),
+      flight_year = lubridate::year(flight_date),
+      failures = as.character(failures)
+    ) %>%
+    dplyr::select(
+      flight_number,
+      flight_name = name,
+      flight_date,
+      flight_year,
+      upcoming,
+      success,
+      failures,
+      crewed,
+      details,
+      id_rocket = rocket,
+      id_launchpad = launchpad
+    )
+  
+}
+
+#' GET 'launchpads' data from the SpaceX API
+#' 
+#' @return A list of the 'launchpads' data points.
+#' 
+get_api_launchpads <- function() {
+  
+  get_api_data(url_launchpads) %>%
+    dplyr::select(
+      id_launchpad = id,
+      launchpad_name = name,
+      launchpad_full_name = full_name,
+      locality,
+      region,
+      timezone,
+      latitude,
+      longitude
+    )
+  
+}
+
+#' GET 'rockets' data from the SpaceX API
+#' 
+#' @return A list of the 'rockets' data points.
+#' 
+get_api_rockets <- function() {
+  
+  get_api_data(url_rockets) %>%
+    dplyr::mutate(
+      engine_number = engines$number,
+      engine_type = engines$type,
+      engine_layout = engines$layout,
+      height_m = height$meters,
+      diameter_m = diameter$meters,
+      mass_kg = mass$kg
+    ) %>%
+    dplyr::select(
+      id_rocket = id,
+      rocket_name = name,
+      rocket_desc = description,
+      engine_number,
+      engine_type,
+      engine_layout,
+      height_m,
+      diameter_m,
+      mass_kg
+    )
   
 }
