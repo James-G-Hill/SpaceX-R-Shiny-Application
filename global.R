07b39d5b1e30ee8aa072ec9d7c135395944a303f
James Hill <j.g.hill@zoho.com>
2021-01-15 02:48:35 +0000

    Removed global file

--- global.R
+++
@@ -1,9 +0,0 @@
-# Libraries --------------------------------------------------------------------
-
-library(curl)
-
-# Magic Numbers ----------------------------------------------------------------
-
-column_width <- 12 # total no. of columns in fluidRow
-
-ggplot_res <- 96 # best resolution for ggplot2 plots
