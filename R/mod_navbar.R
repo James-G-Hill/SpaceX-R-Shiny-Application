4904afdc6a4d1789a16be47a679f2a05c1262759
James Hill <j.g.hill@zoho.com>
2021-11-08 00:26:23 +0000

    Improved branding in dashboard header

--- R/mod_navbar.R
+++ R/mod_navbar.R
@@ -5,12 +5,24 @@
 #' 
 mod_navbar_ui <- function(id) {
   
-  bs4Dash::dashboardHeader(
-    title =
-      bs4Dash::dashboardBrand(
-        title = "SpaceX",
-        color = "primary"
+  brand <-
+    shiny::tags$a(
+      class = "brand-link",
+      href = "#",
+      shiny::tags$img(
+        src = file.path("www", "favicon.png"),
+        class = "brand-image",
+        style = htmltools::css(opacity = 1)
       ),
+      shiny::tags$span(
+        "SpaceX",
+        class = "brand-text font-weight-light",
+        style = htmltools::css(color = "black")
+      )
+    )
+  
+  bs4Dash::dashboardHeader(
+    title = brand,
     compact = TRUE,
     border = FALSE
   )
